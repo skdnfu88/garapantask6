a18aa4c42364df08116830fe030f7e46d2b4b385
skdnfu88 <skdnfu88@gmail.com>
2024-07-30 09:16:47 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x29a20e6013676a8A604e4CD673015B4708C92592'
+export const ProxyAdmin = '0x607804D1CCA69108411Ea9AD6a7A0e893a3418C4'
+export const SWTRImplementation = '0xE81759dCA8aCaAC8beBEA1F61161F93f3993b4B0'
     
